41af2d2cd6e48a05122655ca689ad899ebe98858
Katie Jahanmir <katietj@gmail.com>
2018-09-17 18:39:42 -0700

    added show method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,20 @@
+TASKS = [{id:1, task:"trash",description: "take trash out to curb",completiondate:"9-19-18"},
+{id:2,task:"clean bathroom",description: "wipe the counter and clean toilet",completiondate:"9-20-18"},
+{id:3,task:"dishes",description: "put dishes in dishwasher",completiondate:"9-21-18"},
+{id:4,task:"cook",description: "make food",completiondate:"9-22-18"}]
+
+
+
 class TasksController < ApplicationController
   def index
+    @tasks = TASKS
   end
+
+  def show
+    id = params[:id]
+    @task = TASKS.find do |task|
+      task[:id] == id.to_i
+    end
+end
+
 end
