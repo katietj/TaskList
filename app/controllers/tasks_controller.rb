ddced7a44dd67d5d55c805aa691e612014d52a9a
Katie Jahanmir <katietj@gmail.com>
2018-09-20 13:25:13 -0700

    added completed functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -20,7 +20,7 @@ class TasksController < ApplicationController
     end
 
     def create
-      @task = Task.new(name: params[:task][:name], description: params[:task][:description], completiondate: params[:task][:completiondate])
+      @task = Task.new(name: params[:task][:name], description: params[:task][:description], completiondate: params[:task][:completiondate], completed?: params[:task][:completed?])
       if @task.save
         redirect_to root_path
       else
@@ -34,7 +34,6 @@ class TasksController < ApplicationController
 
     def update
       @task = Task.find_by(id: params[:id].to_i)
-
       @task.name =  params[:task][:name]
       @task.description = params[:task][:description]
       @task.completiondate =  params[:task][:completiondate]
@@ -49,5 +48,19 @@ class TasksController < ApplicationController
        @task = Task.find(params[:id])
        @task.destroy
        redirect_to root_path
+    end
+
+    def complete
+      task = Task.find(params[:id])
+      task.completiondate = Time.now
+      task.save
+      redirect_to root_path
+    end
+
+    def undo_complete
+      task = Task.find(params[:id])
+      task.completiondate = " "
+      task.save
+      redirect_to root_path
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,15 @@ Rails.application.routes.draw do
 
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
   post '/tasks', to: 'tasks#create'
-
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   get '/tasks/:id', to: 'tasks#show', as: 'task'
 
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+  patch '/tasks/:id/complete', to: 'tasks#complete', as: 'complete_task'
+  patch 'tasks/:id/undo_complete', to: 'tasks#undo_complete', as: 'undo_complete'
   patch '/tasks/:id', to: 'tasks#update'
   delete '/tasks/:id', to: 'tasks#destroy', as: 'destroy_task'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
